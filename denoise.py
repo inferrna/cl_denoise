5f65a2e4dcb0c078b09a50f803380cba63fa15dc
inferrna <inferrna@gmail.com>
2015-10-29 14:00:55 +0800

    +tpl is almost done

--- denoise.py
+++ denoise.py
@@ -29,49 +29,34 @@ tplsrc = """
 #define h x[7]
 <% radius = len(rads[0]) %>
 <% rads_cnt = len(rads) %>
+<% from math import sqrt %>
 
-${dtype} fstval(const ${dtype} x[8]){
+${dtype} fstval(const ${dtype} x[${radius}]){
     static const ${dtype} c0 = 1. / sqrt((${dtype})2.) * sqrt((${dtype})(2. / ${radius}.));
-% for i in range(1,radius):
+% for i in range(1,radius+2):
     static const ${dtype} c${i} = cos((${dtype})(M_PI * ${i}. / ${2*radius}.)) * sqrt((${dtype})(2. / ${radius}.));
 % endfor
-    return a*c0 + b*c1 + c*c2 + d*c3 + e*c4 + f*c5 + g*c6 + h*c7;
-    /*X[1] = a*c0 + b*c3 + c*c6 - d*c7 - e*c4 - f*c1 - g*c2 - h*c5;
-    X[2] = a*c0 + b*c5 - c*c6 - d*c1 - e*c4 + f*c7 + g*c2 + h*c3;
-    X[3] = a*c0 + b*c7 - c*c2 - d*c5 + e*c4 + f*c3 - g*c6 - h*c1;
-    X[4] = a*c0 - b*c7 - c*c2 + d*c5 + e*c4 - f*c3 - g*c6 + h*c1;
-    X[5] = a*c0 - b*c5 - c*c6 + d*c1 - e*c4 - f*c7 + g*c2 - h*c3;
-    X[6] = a*c0 - b*c3 + c*c6 + d*c7 - e*c4 + f*c1 - g*c2 + h*c5;
-    X[7] = a*c0 - b*c1 + c*c2 - d*c3 + e*c4 - f*c5 + g*c6 - h*c7;*/
+    return ${' '.join(['{1}*x[{0}]'.format(c, j) for c,j in enumerate(allc[n])])};
 }
 
 
-${dtype} highfq(const ${dtype} x[8]) {
+${dtype} highfq(const ${dtype} x[${radius}]) {
     static const ${dtype} c0 = 1. / sqrt((${dtype})2.) * sqrt((${dtype})(2. / ${radius}.));
-% for i in range(1,radius):
+% for i in range(1,radius+2):
     static const ${dtype} c${i} = cos((${dtype})(M_PI * ${i}. / ${2*radius}.)) * sqrt((${dtype})(2. / ${radius}.));
 % endfor
-    return a*c7 - b*c5 + c*c3 - d*c1 + e*c1 - f*c3 + g*c5 - h*c7;
+    //return a*c7 - b*c5 + c*c3 - d*c1 + e*c1 - f*c3 + g*c5 - h*c7;
+    return ${' '.join(['{1}*x[{0}]'.format(c, j) for c,j in enumerate(allct[radius-1])])};
 }
 
-void dct_ii_8a(const ${dtype} x[8], ${dtype} X[8]) {
+void dct_ii_${radius}a(const ${dtype} x[${radius}], ${dtype} X[${radius}]) {
     static const ${dtype} c0 = 1. / sqrt((${dtype})2.) * sqrt((${dtype})(2. / ${radius}.));
-% for i in range(1,radius):
+% for i in range(1,radius+2):
     static const ${dtype} c${i} = cos((${dtype})(M_PI * ${i}. / ${2*radius}.)) * sqrt((${dtype})(2. / ${radius}.));
 % endfor
 % for i in range(0,radius):
     X[${i}] = ${' '.join(['{1}*x[{0}]'.format(c, j) for c,j in enumerate(allct[i])])};
 % endfor
-/*
-    X[0] = a*c0 + b*c0 + c*c0 + d*c0 + e*c0 + f*c0 + g*c0 + h*c0;
-    X[1] = a*c1 + b*c3 + c*c5 + d*c7 - e*c7 - f*c5 - g*c3 - h*c1;
-    X[2] = a*c2 + b*c6 - c*c6 - d*c2 - e*c2 - f*c6 + g*c6 + h*c2;
-    X[3] = a*c3 - b*c7 - c*c1 - d*c5 + e*c5 + f*c1 + g*c7 - h*c3;
-    X[4] = a*c4 - b*c4 - c*c4 + d*c4 + e*c4 - f*c4 - g*c4 + h*c4;
-    X[5] = a*c5 - b*c1 + c*c7 + d*c3 - e*c3 - f*c7 + g*c1 - h*c5;
-    X[6] = a*c6 - b*c2 + c*c2 - d*c6 - e*c6 + f*c2 - g*c2 + h*c6;
-    X[7] = a*c7 - b*c5 + c*c3 - d*c1 + e*c1 - f*c3 + g*c5 - h*c7;
-*/
 }
 
 __kernel void filter(__global ${dtype} *gdatain, __global ${dtype} *gdataout, __global uint *gminis){
@@ -79,8 +64,8 @@ __kernel void filter(__global ${dtype} *gdatain, __global ${dtype} *gdataout, __
     size_t gx = get_global_id(1) + ${radius}; //With offset
     size_t idx = gy*${w} + gx;
     size_t x, y, mini = 0;
-    ${dtype} data[8];
-    ${dtype} dctf[8];
+    ${dtype} data[${radius}];
+    ${dtype} dctf[${radius}];
     ${dtype} dcmin = INFINITY;
     ${dtype} dccurrent;
 
@@ -101,13 +86,10 @@ __kernel void filter(__global ${dtype} *gdatain, __global ${dtype} *gdataout, __
         data[j] = (${dtype}) 1.0*gdatain[idx + ${w}*y + x];
     }
     gminis[idx] = mini;
-    dct_ii_8a(data, dctf);
-    dctf[7] /= 4;
-    dctf[6] /= 3.5;
-    dctf[5] /= 3;
-    dctf[4] /= 2.5;
-    dctf[3] /= 2;
-    dctf[2] /= 1.5;
+    dct_ii_${radius}a(data, dctf);
+% for i in range(3, radius):
+    dctf[${i}] /= ${i};
+% endfor    
     gdataout[idx] = clamp((${dtype})fstval(dctf), (${dtype})0.0, (${dtype})1.0);
 }
 """
@@ -121,15 +103,16 @@ def arr_from_np(queue, nparr):
     return clarray.Array(queue, nparr.shape, nparr.dtype, data=buf)
 
 
-def get_radius(r, angle):
+def get_radius(n, r, angle):
     res = []
-    for i in range(r):
+    for i in range(-n, r-n):
         x = cos(2*pi*angle/360)*i
         y = sin(2*pi*angle/360)*i
         res.append((round(y), round(x)))
     return res
 
-rr = 8
+rr = 12
+nn = 3
 denom = 2*rr
 allc = []
 for n in range(rr):
@@ -150,7 +133,7 @@ allct = np.array(allc).T.tolist()
 
 rads = []
 for angle in range(0, 360, 5):
-    rads.append(get_radius(rr, angle))
+    rads.append(get_radius(nn, rr, angle))
 
 datal = cv2.imread("../cvrecogn/cicada_molt_stereo_pair_by_markdow.jpg")[:,:,0]/255
 
@@ -166,7 +149,7 @@ gminiscl = clarray.zeros_like(datalcl).astype(np.uint32)
 
 h, w = datalcl.shape
 
-ksource = tpl.render(rads=rads, w=w, allc=allc, allct=allct, dtype='float')
+ksource = tpl.render(rads=rads, w=w, allc=allc, allct=allct, n=nn, dtype='float')
 print(ksource)
 
 
